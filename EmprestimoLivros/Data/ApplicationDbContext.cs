2ddca1301b7471f1d6d88b62487d20aba5752176
Soffi <Soffi@POSITIVOJO>
2024-12-22 12:17:09 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opitions) : base(opitions)
+        {
+        }
+
+
+        public DbSet<EmprestimosModel> EmprestimosModels { get; set; }
+
+
+
+
+    }
+}
---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,16 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+
+        public int Id { get; set; } 
+
+        public string Receptor { get;  set; }
+
+        public string Fornecedor { get; set; }
+
+        public string livroEmprestado { get; set; }
+
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;    
+    }
+}
